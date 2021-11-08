5cd5b796818d1c91efb1f2c81a0bda96dc015752
Ryan Funduk <ryan@funduk.ca>
2021-11-08 15:48:15 -0500

    update site for... err... the "launch" of sr

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -13,23 +13,23 @@ window.twiddler = function() {
   }
 }
 
-window.comingSoon = function() {
-  const comingSoonMsgs = [
-    "soonish",
-    "sometime",
-    "eventually",
-    "at some point",
-    "whenever"
-  ]
-  return {
-    msg: comingSoonMsgs[0],
-    current: 0,
-    nextMsg() {
-      this.current++
-      this.msg = comingSoonMsgs[this.current % comingSoonMsgs.length]
-    },
-    init() {
-      setInterval(this.nextMsg.bind(this), 1100)
-    }
-  }
-}
+// window.comingSoon = function() {
+//   const comingSoonMsgs = [
+//     "soonish",
+//     "sometime",
+//     "eventually",
+//     "at some point",
+//     "whenever"
+//   ]
+//   return {
+//     msg: comingSoonMsgs[0],
+//     current: 0,
+//     nextMsg() {
+//       this.current++
+//       this.msg = comingSoonMsgs[this.current % comingSoonMsgs.length]
+//     },
+//     init() {
+//       setInterval(this.nextMsg.bind(this), 1100)
+//     }
+//   }
+// }
